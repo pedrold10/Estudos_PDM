972593c31f09931e1edc34bee1e6d1e8431bf40f
pedrold10 <pedrogabriel12423@gmail.com>
2022-08-19 15:50:40 -0300

    Adicionando novo projeto

--- Geraldinho/app/src/main/java/com/example/geraldinho/MainActivity.kt
+++ Geraldinho/app/src/main/java/com/example/geraldinho/MainActivity.kt
@@ -1,13 +1,19 @@
-package com.example.initialapp
+package com.example.geraldinho
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etQuantidade: EditText
+    private lateinit var tvNumeros: TextView
+    private lateinit var btGerar: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
     }
 }
